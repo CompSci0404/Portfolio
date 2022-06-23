acf03144fb70b55449c4ee7d3e5a2f527294d612
Matt <mtradke@sasktel.net>
2022-06-23 16:27:05 -0600

    basic front page layout crafted.

---
+++ MainPage/MainPageHandler.js
@@ -0,0 +1,21 @@
+
+var nameOfWebsite = "Matt Radke's Portfolio";
+
+window.onload = function()
+{
+    var AllTitles = document.getElementsByClassName("title"); 
+    var counter = 0;
+    
+    while(counter < AllTitles.length)
+    {
+        AllTitles[counter].innerHTML = nameOfWebsite;
+        counter ++; 
+    }
+
+   // document.getElementById("title").innerHTML = nameOfWebsite; 
+}
+
+function SendAlert()
+{
+    alert("test");
+}
